d093699c1b711448b376c82295fc400df93afc7a
CrypticSwarm <crypticswarm+git@gmail.com>
2014-07-19 17:01:26 -0500

    pass output file as argument

--- file.js
+++ file.js
@@ -5,5 +5,4 @@ var escodegen = require('escodegen').generate
 var fs = require('fs')
 var contents = fs.readFileSync(process.argv[2]).toString()
 
-console.log(process.argv[2])
-fs.writeFileSync('output.js', escodegen(convert(esprima(contents))[0]))
+fs.writeFileSync(process.argv[3], escodegen(convert(esprima(contents))[0]))
